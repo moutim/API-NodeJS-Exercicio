693994dda2e1fb2c065d2973c65376c45755bca7
unknown <moutimg@gmail.com>
2022-01-24 11:57:03 -0300

    Arquivo para conexao no banco criada e configurada

---
+++ src/banco.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2';
+
+const dadosConexao = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'escola'
+};
+const conexao = mysql.createConnection(dadosConexao);
+
+conexao.connect(erro => {
+    (erro) ? console.error(`Erro ao conectar no banco: ${erro.message}`) : console.log('Banco de dados conectado com sucesso');
+});
