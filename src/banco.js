187da6352cb480fa656a83231960d041f6fd9604
unknown <moutimg@gmail.com>
2022-01-25 09:05:00 -0300

    Exporta funcao banco

--- src/banco.js
+++ src/banco.js
@@ -11,3 +11,5 @@ const conexao = mysql.createConnection(dadosConexao);
 conexao.connect(erro => {
     (erro) ? console.error(`Erro ao conectar no banco: ${erro.message}`) : console.log('Banco de dados conectado com sucesso');
 });
+
+export default conexao;
